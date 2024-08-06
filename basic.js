6057d6cd6a0da8002716bb4951300198cf0c44d6
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2024-08-06 19:03:23 +0530

    javascript 1

---
+++ basic.js
@@ -0,0 +1,5 @@
+let namee ="aakash yadav";
+const mobileNumber = 9729689635;
+let course = "Java script";
+//the next instruction will be able to print multiple variale at the same time
+console.table([namee,mobileNumber,course]);
