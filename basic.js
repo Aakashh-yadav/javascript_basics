a725d3e0b2ff52183c64f0bd6206e6f056fa93ed
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2024-08-06 19:06:32 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -2,4 +2,6 @@ let namee ="aakash yadav";
 const mobileNumber = 9729689635;
 let course = "Java script";
 //the next instruction will be able to print multiple variale at the same time
+
+//please dont use var keyword to make variables because this is not having any kind of block scope  and functional scope
 console.table([namee,mobileNumber,course]);
