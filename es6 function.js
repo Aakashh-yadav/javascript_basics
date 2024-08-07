f922b6f8538463a4ece95503307b041c9e6db573
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2024-08-07 08:36:29 +0530

    for of

---
+++ es6 function.js
@@ -0,0 +1,10 @@
+const cars=["BMW","MG","AUDI"];
+let k=[];
+for (const x of cars) {
+    //the following line will be for appending the values of cars array in string K
+    //k.pop(x);  
+    k.push(x);
+     
+
+}
+console.log(k);
