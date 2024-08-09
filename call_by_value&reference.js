cbb4d1236c792c0f7b1189279169f5e73a7548d7
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2024-08-09 22:20:19 +0530

    call by value

---
+++ call_by_value&reference.js
@@ -0,0 +1,18 @@
+// let x=22;
+
+// let a =function func(i){
+//     return console.log(`the value of function  is ${i}`);
+    
+// }
+
+
+// console.log(a());
+ 
+let x = 10;
+
+function modifyValue(val) {
+    val = 20;
+}
+
+modifyValue(x);
+console.log(x); // Outputs: 10
