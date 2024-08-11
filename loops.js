80e1f91bb942b3ecd8bb1153e75b7a7559e5cdc9
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2024-08-11 07:17:17 +0530

    day 5

---
+++ loops.js
@@ -0,0 +1,9 @@
+let arr=[1,2,3,4]
+// for (const ele of arr) {
+//     console.log(ele*2)
+    
+// }
+for (const key in arr) {
+   console.log(arr[key]*2)
+        
+    }
--- map,filter,reduce.js
+++ map,filter,reduce.js
@@ -1,22 +1,27 @@
-// // map
-// arr=[55,87,41,62]
-// let a=arr.map((ele ,index,arr)=>{
-//     console.log(ele,index);
+// map
+arr=[55,87,41,62]
+let a=arr.map((ele ,index,arr)=>{
+    console.log(ele,index);
     
-//     return ele*2;
-// })
-// console.log(a);
-// console.log(arr);
+    return ele*2;
+})
+console.log(a);
+console.log(arr);
 
-//for each
-// arr.forEach((ele) => {
-//     console.log(ele*2);    
-// });
-
-//filter
-//  arr=[55,87,41,62,8,6,51,2]
-//  var a =arr.filter((a)=>{
-//     return a<50;
-//  })
-//  console.log(a);
- 
+// for each
+arr.forEach((ele) => {
+    console.log(ele*2);    
+});
+// 
+// filter
+ arr=[55,87,41,62,8,6,51,2]
+ var a =arr.filter((a)=>{
+    return a<50;
+ })
+ console.log(a);
+// Reduce 
+arr=[1,2,3,5,6]
+var a=arr.reduce((acc,ele)=>{
+    return acc+ele;
+},0)
+console.log(a);
