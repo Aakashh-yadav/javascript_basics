5dcf00c4b5d422e9ed95ce479059862b782ff410
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2024-11-06 23:45:23 +0530

    yess

--- objects.js
+++ objects.js
@@ -8,5 +8,15 @@
 // console.log(obj1);
 
 // // console.log(this[mysym])
-const one = new String('aakash-yadav')
-console.log(one.lastIndexOf('a'));
+// const one = new String('aakash-yadav')
+// console.log(one.lastIndexOf('a'));
+// let min=10;
+// let max= 20;
+// console.log(Math.floor(Math.random()*(max-min+1))+min)
+let my_date = new Date()
+console.log(my_date);
+console.log(my_date.toLocaleString());
+console.log(my_date.toJSON());
+console.log(my_date.toLocaleTimeString());
+console.log(my_date.toTimeString());
+console.log(my_date.getTimezoneOffset());
